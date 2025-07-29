f5546849db99d867592452fa875970be0261937e
Igor Bebiava <drprime8@gmail.com>
2025-07-29 21:58:20 +0300

    GoogleCloudStorage integration

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import os
 import time
 from dotenv import load_dotenv
+from google.cloud import storage
 
 from app.smithery import SmithreApi
 
@@ -9,9 +10,13 @@ from app.smithery import SmithreApi
 def main():
     load_dotenv()
     api_key = os.getenv("SMITHERY_API_KEY")
-    update_freq = int(os.getenv("SMITHERY_API_KEY")) * 3600
+    bucket_name = os.getenv("STORAGE_BACKET_NAME")
+    bucket_folder = os.getenv("STORAGE_BACKET_FOLDER")
+    storage_account_key = os.getenv("STORAGE_SERVICE_ACCOUNT_KEY")
+    update_freq = int(os.getenv("SMITHERY_UPDATE_FREQ")) * 3600
+    
     app = SmithreApi(api_key)
-    recheck_time = 72*60*60
+    storage_client = storage.Client.from_service_account_json(storage_account_key)
     
     if os.path.exists("result.json"):
         with open("result.json", "r") as file:
@@ -48,11 +53,18 @@ def main():
         if not server_data["exists"]:
             print("Server doesn't exists")
             continue
+        
         previous_results["servers"][server_name]["logo"] = server_data["iconUrl"]
         previous_results["servers"][server_name]["tools"] = server_data["tools"]
         previous_results["servers"][server_name]["settings"] = server_data["settings"]
         previous_results["servers"][server_name]["original_api_data"] = server_data
-        previous_results["servers"][server_name]["updated_at"] = timestamp # TODO current timestamp(ms)
+        previous_results["servers"][server_name]["updated_at"] = timestamp
+        
+        filename = server_name.replace("/", "_")
+        destination_file_name = f"{bucket_folder}/{filename}.json"
+        blob = storage_client.bucket(bucket_name).blob(destination_file_name)
+        blob.upload_from_string(json.dumps(previous_results["servers"][server_name]), content_type='application/json')
+        print(f"File uploaded to gs://{bucket_name}/{destination_file_name}")
         
         if index % 20 == 0:
             with open("result.json", "w+") as file:
@@ -60,14 +72,8 @@ def main():
         
         time.sleep(1)
         
-        
     with open("result.json", "w+") as file:
         json.dump(previous_results, file)
-    
-    servers_list_save = [ previous_results["servers"][server_key] for server_key in previous_results["servers"] ]
-    
-    with open("result_list.json", "w+") as file:
-        json.dump(servers_list_save, file)
     
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -11,13 +11,19 @@ pip install -r requirements.txt
 ### Setup API key
 ```env
 SMITHERY_API_KEY=...
+
 ```
 
 ### Setup update frequency in hours
 ```env
 SMITHERY_UPDATE_FREQ=72
 ```
-
+### Setup GoogleConsole Service account key file and bucket settings
+```env
+STORAGE_SERVICE_ACCOUNT_KEY=key.json
+STORAGE_BACKET_NAME=...
+STORAGE_BACKET_FOLDER=...
+```
 
 ## Usage
 ### Run:
@@ -25,4 +31,4 @@ SMITHERY_UPDATE_FREQ=72
 python main.py
 ```
 
-Results will be saved to in ``result_list.json`` as list and ``result.json`` file as map
+Results will be saved to GoogleCloudStorage and to ``result.json`` file as map
